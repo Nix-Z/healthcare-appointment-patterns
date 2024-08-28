4d1f22809da0c0bc7aaf21b2dc3037d1fcb1b6ee
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-08-27 17:08:09 -0700

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,10 +4,6 @@ from data_analysis import analyze_data
 def preprocess_data():
     data, categorical_features, numerical_features = analyze_data()
 
-    # Drop unnecessary rows 
-    data.drop(['Patient_ID'],
-              axis=1, inplace=True)
-
     # Remove rows with any empty values
     data = data.dropna().reset_index(drop=True)
 
@@ -18,12 +14,18 @@ def preprocess_data():
     # Convert 'Booking_Date' and 'Appointment_Date' to datetime objects
     data['Booking_Date'] = pd.to_datetime(data['Booking_Date'], format="%Y-%m-%d")
     data['Appointment_Date'] = pd.to_datetime(data['Appointment_Date'], format="%Y-%m-%d")
-  
+
+    # Calculate the difference in days between 'Booking_Date' and 'Appointment_Date'
+    data['Days_Between'] = (data['Appointment_Date'] - data['Booking_Date']).dt.days
+
+    # Drop unnecessary rows 
+    data.drop(['Patient_ID', 'Booking_Date', 'Appointment_Date'], axis=1, inplace=True)
+
     print(data.head())
-    
+
     categorical_features = data.select_dtypes("object").columns
     numerical_features = data.select_dtypes("number").columns
-  
+
     return data, categorical_features, numerical_features
 
 preprocess_data()
